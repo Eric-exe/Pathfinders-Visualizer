eb57823fc1627c8c8b6160ed67cad30a072753bb
Rinciel <Rinciel@users.noreply.github.com>
2023-04-20 20:45:47 +0000

    deploy: 6920becf3afd89c3166009dce00c6ac693ee3af7

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-function main() {
-    console.log("Test");
+const CELL_PIXELS = 20;
+ROWS = -1;
+COLS = -1;
+
+function init() {
+    ROWS = Math.floor(windowWidth / CELL_PIXELS); // how many cells are in a row
+    COLS = Math.floor(windowHeight / CELL_PIXELS); // how many cells are in a column
+}
+
+function setup() {
+    init();
+    canvas = createCanvas(ROWS * CELL_PIXELS, COLS * CELL_PIXELS);
+    canvas.style('display', 'block');
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    init();
+}
+
+// ============================================================================================
+// all functions pertaining to the maze
+function displayMaze() {
+    drawGrid();
+}
+
+function draw() {
+    background(220);
+    displayMaze();
 }
