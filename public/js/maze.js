c4917becab5984b8bc1ea28e6af5212a8bc219ea
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2023-04-21 01:41:26 -0400

    Implemented BFS

--- public/js/maze.js
+++ public/js/maze.js
@@ -1,14 +1,10 @@
 // ===========================================================================================
 // maze vars
 let maze;
-let beginCell;
-let endCell;
-// ===========================================================================================
-// these variables help visualize the maze being built
-let primSteps = [];
-let primStepCounter = 0;
-// ===========================================================================================
+let beginCell; // [xCoord][yCoord]
+let endCell; // [xCoord][yCoord]
 
+// Creates the maze with simplified Prim's algorithm. Also sets the start and end cells
 function mazeInit() {
     maze = [];
 
@@ -16,18 +12,22 @@ function mazeInit() {
         maze[i] = [];
         for (let j = 0; j < COLS; j++) {
             maze[i][j] = [ 
-                "wall", // type
+                "wall", // state
                 "wall", // original state, we will need it when the user wants to reset the maze
                 CELL_PIXELS * j + j, // x coord
                 CELL_PIXELS * i + i, // y coord
             ];
         }
     }
-    
-    primStepCounter = 0;
-    primSteps = prim();
 
-    // Add begin and end
+    // now generate the maze
+    prim();
+    primAnim = false; // the animation did not run yet
+
+    // now solve the maze
+    bfs();
+    solverAnim = false;
+    
 
     return maze;
 }
@@ -66,11 +66,17 @@ function drawCells() {
                 case "path":
                     fillColor = PATH_COLOR;
                     break;
-                case 'begin':
+                case "begin":
                     fillColor = BEGIN_COLOR;
                     break;
-                case 'end':
+                case "end":
                     fillColor = END_COLOR;
+                    break;
+                case "explored":
+                    fillColor = EXPLORED_COLOR;
+                    break;
+                case "res":
+                    fillColor = RES_COLOR;
                     break;
             }
 
@@ -80,26 +86,14 @@ function drawCells() {
     }
 }
 
+// ===========================================================================================
+// Animation states
+let primAnim = false;
+let solverAnim = false;
+// ===========================================================================================
 function drawMaze() {
     drawGrid();
-
-    // animate the building of the maze
-    if (primStepCounter < primSteps.length) {
-
-        let coords = primSteps[primStepCounter];
-
-        if (SKIP_PRIM_ANIM) {
-            for (; primStepCounter < primSteps.length; primStepCounter++) {
-                coords = primSteps[primStepCounter];
-                maze[coords[0]][coords[1]][0] = maze[coords[0]][coords[1]][1];
-            }
-        }
-
-        else if (primStepCounter < primSteps.length) {
-            maze[coords[0]][coords[1]][0] = maze[coords[0]][coords[1]][1];
-            primStepCounter++;
-        }
-    }
-
+    if (!primAnim) primAnim = animatePrim();
+    else if (!solverAnim) solverAnim = animateBFS();
     drawCells();
 }
