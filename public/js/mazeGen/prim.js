cf1d728fe88671d8ed90b1a46e9604e475f32eef
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2023-04-23 03:15:27 -0400

    Rewrote Prim's algorithm

--- public/js/mazeGen/prim.js
+++ public/js/mazeGen/prim.js
@@ -30,7 +30,7 @@ function prim() {
     mazeGenSteps = [];
     mazeGenCounter = 0;
     
-    generateMaze(x, y);
+    generatePrimMaze(x, y);
     
     if (mazeGenSteps.length < 2) return;
     // choose two random steps to be the begin and end cells
@@ -45,26 +45,34 @@ function prim() {
     maze[endCell[0]][endCell[1]][1] = 'end';
 }
 
-let unexplored = [];
-
-function generateMaze(x, y) {
+function generatePrimMaze(x, y) {
     maze[x][y][1] = "path";
     mazeGenSteps.push([x, y]);
 
-    if (x >= 2 && maze[x - 2][y][1] == "wall") unexplored.push([x - 1, y, x - 2, y]);
-    if (y >= 2 && maze[x][y - 2][1] == "wall") unexplored.push([x, y - 1, x, y - 2]);
-    if (x < ROWS - 2 && maze[x + 2][y][1] == "wall") unexplored.push([x + 1, y, x + 2, y]);
-    if (y < COLS - 2 && maze[x][y + 2][1] == "wall") unexplored.push([x, y + 1, x, y + 2]);
+    let unexplored = [];
 
-    while (unexplored.length != 0) {
+    while (true) {
+        if (x >= 2 && maze[x - 2][y][1] == "wall") unexplored.push([x - 1, y, x - 2, y]);
+        if (y >= 2 && maze[x][y - 2][1] == "wall") unexplored.push([x, y - 1, x, y - 2]);
+        if (x < ROWS - 2 && maze[x + 2][y][1] == "wall") unexplored.push([x + 1, y, x + 2, y]);
+        if (y < COLS - 2 && maze[x][y + 2][1] == "wall") unexplored.push([x, y + 1, x, y + 2]);
+
+        if (unexplored.length == 0) break;
+
         let idx = Math.floor(Math.random() * unexplored.length);
         let coords = unexplored[idx];
         unexplored.splice(idx, 1);
 
         if (maze[coords[2]][coords[3]][1] == "wall") {
+            
             maze[coords[0]][coords[1]][1] = "path";
             mazeGenSteps.push([coords[0], coords[1]]);
-            generateMaze(coords[2], coords[3]);
+
+            maze[coords[2]][coords[3]][1] = "path";
+            mazeGenSteps.push([coords[2], coords[3]]);
+
+            x = coords[2];
+            y = coords[3];
         }
     }
 }
