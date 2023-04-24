a08772ca2e7bc19cc44f5e0c7807709d15adc5cd
Rinciel <Rinciel@users.noreply.github.com>
2023-04-24 22:28:45 +0000

    deploy: 89351ffe9a8830e8e7dcb549ca8f9c1d16981af7

---
+++ js/menuElements/button.js
@@ -0,0 +1,46 @@
+const BUTTON_BG_COLOR = [13, 17, 23, TRANSPARENCY];
+const BUTTON_BORDER_COLOR = [213, 217, 223, TRANSPARENCY];
+const BUTTON_TEXT_COLOR = [255, 255, 255, TRANSPARENCY];
+const BUTTON_OVER_BORDER_COLOR = [30, 144, 255, TRANSPARENCY];
+
+class Button {
+    constructor(text) {
+        this.text = text;
+
+        this.width = 100;
+        this.height = 20;
+    }
+
+    draw(x, y) {
+        this.x = x;
+        this.y = y;
+        
+        // highlight if cursor is over it
+        fill(BUTTON_BG_COLOR);
+        stroke(BUTTON_BORDER_COLOR);
+
+        if (mouseX > this.x &&
+            mouseY > this.y &&
+            mouseX < this.x + this.width &&
+            mouseY < this.y + this.height) {
+                stroke(BUTTON_OVER_BORDER_COLOR);
+            }
+
+        rect(this.x, this.y, this.width, this.height);
+
+
+        let textW = textWidth(this.text);
+        fill(BUTTON_TEXT_COLOR);
+        stroke(BUTTON_TEXT_COLOR);
+        text(this.text, this.x + (this.width - textW) / 2, this.y + 13);
+    }
+
+    mousePressed() {
+        if (mouseX > this.x &&
+            mouseY > this.y &&
+            mouseX < this.x + this.width &&
+            mouseY < this.y + this.height) {
+            console.log("Button pressed");
+        }
+    }
+}
