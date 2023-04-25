74f89ed942d48dedac94a531c4043f8d65750085
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2023-04-24 22:13:47 -0400

    Minor tweaks

--- public/script.js
+++ public/script.js
@@ -2,20 +2,12 @@ let FRAMERATE = 60; // some browsers might not support framerates. Max framerate
 
 const BG_COLOR = [153, 157, 163];
 
-function init() {
-    frameRate(FRAMERATE);
-    menuInit();
-}
-
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.style('display', 'block');
-    init();
-}
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    init();
+    frameRate(FRAMERATE);
+    menuInit();
 }
 
 // ============================================================================================
